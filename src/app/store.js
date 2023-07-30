38c3bc247e5a5c589f0c2056b98cbf1de6447e99
GRE3DISGOOD <gaetanhonthaas@gmail.com>
2023-07-30 16:33:57 +0200

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import languageReducer from "../feature/language.slice";
-
-export default configureStore({
-  reducer: {
-    language: languageReducer,
-  },
-});
